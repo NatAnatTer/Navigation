7ffb5343496a0a46b2e83dce2567752bc789d658
Natalia <natinho@yandex.ru>
2022-12-14 00:56:07 +0300

    add profile header view

---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Наталья Терзьян on 08.12.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    let profileHeader = ProfileHeaderView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .lightGray
+        self.navigationItem.title = "Профиль"
+        
+        self.view.addSubview(profileHeader)
+       }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        profileHeader.frame = view.frame
+        
+        /*
+         Создайте экземпляр класса ProfileHeaderView в классе ProfileViewController, добавьте его в качестве subview и в методе viewWillLayoutSubviews() задайте ему frame, равный frame корневого view.
+         */
+    }
+    
+    
+   
+}
+
+extension UIResponder {
+    func responderChain() -> String{
+        guard let next = next else {
+            return String(describing: Self.self)
+        }
+        return String(describing: Self.self) + " -> " + next.responderChain()
+    }
+}
+
+
+
+
+
