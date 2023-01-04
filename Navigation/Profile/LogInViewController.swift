2dc6b23210bc3f24333e0006fc982fc9ee836c7e
Natalia <natinho@yandex.ru>
2023-01-04 23:22:17 +0300

    start commit UITable Scroll part1

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Наталья Терзьян on 04.01.2023.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+  
+}
