9038f6ee5dc9ae6af31492b2559612479882ad66
Natalia <natinho@yandex.ru>
2023-01-23 23:02:17 +0300

    create photos view controller

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ProfileViewController: UIViewController{
     
     let arrayOfPost:[Post] = [postOne, postTwo, postThree, postFour]
+    let arrayOfPhoto:[String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
     
     let postLine: UITableView = {
         let view = UITableView(frame: .zero, style: .plain)
@@ -37,7 +38,7 @@ class ProfileViewController: UIViewController{
         self.postLine.dataSource = self
        // self.postLine.register(ProfileHeaderView.self, forHeaderFooterViewReuseIdentifier: ProfileHeaderView.id)
         self.postLine.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.identifire)
-        
+        self.postLine.register(PhotosTableViewCell.self, forCellReuseIdentifier: PhotosTableViewCell.identifire)
         self.postLine.tableHeaderView = ProfileHeaderView()
     }
 }
@@ -56,24 +57,55 @@ extension ProfileViewController:  UITableViewDelegate, UITableViewDataSource{
         ])
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayOfPost.count
+        if section == 0{
+            return arrayOfPhoto.count
+        } else{
+            return arrayOfPost.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 200
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = postLine.dequeueReusableCell(withIdentifier: PostTableViewCell.identifire, for: indexPath) as! PostTableViewCell
-        let postList = arrayOfPost[indexPath.row]
-        cell.authorOfPost.text = postList.author
-        cell.contentOfPost.image = UIImage(named: postList.imageOfPost)
-        cell.descriptionOfPost.text = postList.descriptionOfPost
-        cell.likesOfPost.text = "Likes: \(postList.likes)"
-        cell.viewsOfPost.text = "Views: \(postList.views)"
         
-        return cell
+        if indexPath.section == 0{
+            let cell = postLine.dequeueReusableCell(withIdentifier: PhotosTableViewCell.identifire, for: indexPath) as! PhotosTableViewCell
+            let photoList = arrayOfPhoto[indexPath.row]
+            cell.contentOfPost.image = UIImage(named: photoList)
+            return cell
+            
+        } else {
+            
+            let cell = postLine.dequeueReusableCell(withIdentifier: PostTableViewCell.identifire, for: indexPath) as! PostTableViewCell
+            let postList = arrayOfPost[indexPath.row]
+            cell.authorOfPost.text = postList.author
+            cell.contentOfPost.image = UIImage(named: postList.imageOfPost)
+            cell.descriptionOfPost.text = postList.descriptionOfPost
+            cell.likesOfPost.text = "Likes: \(postList.likes)"
+            cell.viewsOfPost.text = "Views: \(postList.views)"
+            
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        postLine.deselectRow(at: indexPath, animated: true)
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         postLine.dequeueReusableHeaderFooterView(withIdentifier: ProfileHeaderView.id)
     }
+    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 50
+//    }
+    
+    
 }
