1b1c4102b285ee8f201dc39f33cd4e3950b8876e
Natalia <natinho@yandex.ru>
2023-01-24 00:13:47 +0300

    in process

---
+++ Navigation/Profile/PhotosCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Navigation
+//
+//  Created by Наталья Терзьян on 23.01.2023.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    static let identifire = "PhotosCollectionViewCell"
+    
+}
--- Navigation/Profile/PhotosViewController.swift
+++ Navigation/Profile/PhotosViewController.swift
@@ -7,23 +7,62 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UICollectionView {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    init(){
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 20, right: 0)
+ 
+        super .init(frame: .zero, collectionViewLayout: layout)
+       
+        
+        register(PhotosCollectionViewCell.self, forCellWithReuseIdentifier: PhotosCollectionViewCell.identifire)
+        
+        contentInset = UIEdgeInsets(top: 16, left: 8, bottom: 8, right: 8)
+        isPagingEnabled = true
+        dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        (self.collectionViewLayout as! UICollectionViewFlowLayout).itemSize = CGSize(width: (UIScreen.main.bounds.width - 32)/3, height: 100)
+    }
 
 }
+
+
+extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+}
+
+
+//let postLine: UICollectionView = {
+//        let view = UICollectionView(frame: .zero, style: .plain)
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.backgroundColor = .white
+//        return view
+//    }()
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setupView()
+//
+//    }
+//
+//    private func setupView() {
+//        self.view.backgroundColor = .systemGray6
+//        self.navigationItem.title = "Профиль"
+//        let safeLayout = self.view.safeAreaLayoutGuide
+////        addAllSubviews()
+////        setupAllViews(safeLayout)
+////        createTable()
+//    }
+//
